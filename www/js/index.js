75a24097597204fa60342e4f8515a0484a2119d4
Luis Anaya <luigui.a@gmail.com>
2019-06-20 16:09:50 -0500

    ertert

--- www/js/index.js
+++ www/js/index.js
@@ -1,36 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    this.bindEvents();
     },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+        app.receivedEvent('deviceready');
 
-    // Update DOM on a Received Event
+        // Here, we redirect to the web site.
+        var targetUrl = "https://escueladenegociosinmobiliarios.com/mipin/";
+        window.location.replace(targetUrl);
+},
+    // Note: This code is taken from the Cordova CLI template.
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -40,9 +23,6 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-		
-		var iab = cordova.InAppBrowser;
-		iab.open('https://escueladenegociosinmobiliarios.com/mipin/', '_self');  
     }
 };
 
